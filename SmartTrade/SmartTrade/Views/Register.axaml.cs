5e1577298abac9315d4239325f26476104f44773
JaviLatorre <javierlatorrecano@gmail.com>
2024-03-25 20:23:59 +0100

    registrar comprador

--- SmartTrade/SmartTrade/Views/Register.axaml.cs
+++ SmartTrade/SmartTrade/Views/Register.axaml.cs
@@ -1,5 +1,17 @@
+using System;
 using Avalonia.Controls;
-
+using Avalonia.Interactivity;
+using SmartTrade.ViewModels;
+using System.Collections.ObjectModel;
+using Avalonia.Controls.Templates;
+using Avalonia.Markup.Xaml.Templates;
+using SmartTradeLib.Entities;
+using Avalonia.Media;
+using Microsoft.IdentityModel.Tokens;
+using ReactiveUI;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.VisualTree;
+using Avalonia;
 namespace SmartTrade.Views
 {
     public partial class Register : UserControl
