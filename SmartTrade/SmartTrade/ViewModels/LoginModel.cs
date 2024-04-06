cde3e971a43c72535986d6cd6c829eeea6aeb498
JaviLatorre <javierlatorrecano@gmail.com>
2024-04-06 17:31:38 +0200

    cambios login

---
+++ SmartTrade/SmartTrade/ViewModels/LoginModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartTrade.ViewModels
+{
+    public class LoginModel : ViewModelBase
+    {
+        string name;
+        string password;
+        public LoginModel(string name, string password)
+        {
+            this.name = name;
+            this.password = password;
+        }
+
+        internal void Login(string email, string password)
+        {
+            MainViewModel.SmartTradeService.LogIn(email, password);
+        }
+    }
+}
--- SmartTrade/SmartTrade/Views/Login.axaml.cs
+++ SmartTrade/SmartTrade/Views/Login.axaml.cs
@@ -1,12 +1,24 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using SmartTrade.ViewModels;
+using System;
 
 namespace SmartTrade.Views
 {
     public partial class Login : UserControl
     {
+        private LoginModel? _model;
         public Login()
         {
             InitializeComponent();
+            SignUpButton.Click += SignUpButton_click;
+        }
+
+        private void SignUpButton_click(object? sender, RoutedEventArgs e)
+        {
+            string email = TextBoxEmail.Text;
+            string password = TextBoxPassword.Text;
+            _model.Login(email, password);
         }
     }
 }
