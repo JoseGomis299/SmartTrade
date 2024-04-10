b438299e7c84eac785bd5322f9fd558d0bd172e3
JaviLatorre <javierlatorrecano@gmail.com>
2024-04-10 17:47:52 +0200

    implementación register

--- SmartTrade/SmartTrade/ViewModels/RegisterModel.cs
+++ SmartTrade/SmartTrade/ViewModels/RegisterModel.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +10,18 @@ namespace SmartTrade.ViewModels
 {
     public class RegisterModel : ViewModelBase
     {
-
-    }
+        public DateTime convertDate(string dateString)
+        {
+            DateTime date;
+            try
+            {
+                date = DateTime.ParseExact(dateString, "dd/MM/yyyy", CultureInfo.InvariantCulture);
+                return date;
+            }
+            catch (FormatException)
+            {
+                throw new Exception("formato incorrecto");
+            }
+        }
+    }        
 }
--- SmartTrade/SmartTrade/Views/Register.axaml.cs
+++ SmartTrade/SmartTrade/Views/Register.axaml.cs
@@ -1,12 +1,89 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using System;
+using SmartTradeLib;
+using SmartTradeLib.Entities;
+using Avalonia.Input;
+using System.Linq;
+using SmartTrade.ViewModels;
+using Splat;
 
 namespace SmartTrade.Views
 {
     public partial class Register : UserControl
     {
+        private RegisterModel _model;
+
         public Register()
         {
             InitializeComponent();
+            SignInButton.Click += SignInButton_click;
+            RegisterSellerButton.Click += RegisterSellerButton_click;
+            LoginButton.Click += LoginButton_click;
+
+        }
+
+        private void LoginButton_click(object? sender, RoutedEventArgs e)
+        {
+            SmartTradeNavigationManager.Instance.NavigateTo(new Login());
+        }
+
+        private void RegisterSellerButton_click(object? sender, RoutedEventArgs e)
+        {
+            SmartTradeNavigationManager.Instance.NavigateTo(new SellerRegister());
+        }
+
+        private void SignInButton_click(object? sender, RoutedEventArgs e)
+        {
+            string name = TextBoxName.Text;
+            string lastnames = TextBoxLastNames.Text;
+            string email = TextBoxEmail.Text;
+            string password = TextBoxPassword.Text;
+            string dni = TextBoxDNI.Text;
+            string province = TextBoxProvince.Text;
+            string municipality = TextBoxMunicipality.Text;
+            string postalCode = TextBoxPostalCode.Text;
+            string street = TextBoxStreet.Text;
+            string number = TextBoxNumber.Text;
+            string door = TextBoxDoor.Text;
+            string dateBirthString = TextBoxDateBirth.Text;
+            try
+            {
+                Address consumerAddress = new Address(province, street, municipality, postalCode, number, door);
+                DateTime dateBirth = _model.convertDate(dateBirthString);
+                _model.RegisterConsumer(email,password,name,lastnames,dni,dateBirth,billingAddress,consumerAddress);
+
+            }
+            catch (Exception ex)
+            {
+                if (ex.Message.Contains("Formato Incorrecto"))
+                {
+                    TextBoxDateBirth.ErrorMessage.BringIntoView();
+                    TextBoxDateBirth.ErrorMessage.Text = ex.Message;
+                }
+            }
+        }
+        private void DateTextBox_TextInput(object sender, TextInputEventArgs e)
+        {
+            var textBox = (TextBox)sender;
+            var text = textBox.Text;
+            var caretIndex = textBox.CaretIndex;
+            if (!char.IsDigit(e.Text[0]))
+            {
+                e.Handled = true;
+            }
+            text = new string(text.Where(c => char.IsDigit(c)).ToArray()).PadRight(8, '0');
+            if (text.Length >= 2)
+            {
+                text = text.Insert(2, "/");
+            }
+            if (text.Length >= 5)
+            {
+                text = text.Insert(5, "/");
+            }
+            textBox.Text = text;
+            textBox.CaretIndex = Math.Min(caretIndex + 1, text.Length);
+            e.Handled = true;
         }
     }
 }
