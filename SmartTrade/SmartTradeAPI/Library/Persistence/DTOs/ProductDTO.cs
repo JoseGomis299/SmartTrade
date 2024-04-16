b771f96485bcf1fbd4011062f03da282772a28ec
JoseGomis299 <joseg03a@gmail.com>
2024-04-16 12:07:46 +0200

    ProducrtInfo

--- SmartTrade/SmartTradeAPI/Library/Persistence/DTOs/ProductDTO.cs
+++ SmartTrade/SmartTradeAPI/Library/Persistence/DTOs/ProductDTO.cs
@@ -7,11 +7,13 @@ public class ProductDTO
     public List<byte[]> Images { get; set; }
     public List<string> Attributes { get; set; }
     public string? Differentiators { get; set; }
+    public string? Info { get; set; }
 
     public ProductDTO(Product product)
     {
         Images = product.Images.Select(i => i.ImageSource).ToList();
         Attributes = product.GetAttributes().ToList();
         Differentiators = product.GetDifferentiations();
+        Info = product.GetInfo();
     }
 }
