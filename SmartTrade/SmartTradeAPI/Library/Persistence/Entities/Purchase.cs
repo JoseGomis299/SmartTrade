01bc5e5158641edd81a5afd77140eea66d9cc4e7
JaviLatorre <javierlatorrecano@gmail.com>
2024-04-24 21:30:38 +0200

    clase purchase

---
+++ SmartTrade/SmartTradeAPI/Library/Persistence/Entities/Purchase.cs
@@ -0,0 +1,15 @@
+﻿using SmartTrade.Entities;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SmartTrade.Entities
+{
+    public abstract partial class Purchase
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Product? PurchaseProduct { get; set; }
+        public int Price { get; set; }
+        public Post? PurchasePost { get; set; }
+        public Seller? PurchaseSeller { get; set; }
+    }
+}
