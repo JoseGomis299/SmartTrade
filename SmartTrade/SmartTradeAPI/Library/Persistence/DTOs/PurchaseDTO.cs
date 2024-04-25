6ca0188079e1cdf5a4c46c1ced5f30d6ece28cfe
JaviLatorre <javierlatorrecano@gmail.com>
2024-04-25 21:55:24 +0200

    DTO PURCHASE

---
+++ SmartTrade/SmartTradeAPI/Library/Persistence/DTOs/PurchaseDTO.cs
@@ -0,0 +1,22 @@
+﻿using SmartTrade.Entities;
+
+namespace SmartTradeAPI.Library.Persistence.DTOs
+{
+    public class PurchaseDTO
+    {
+        public int Precio { get; set; }
+        public int? Idproducto { get; set; }
+        public string? EmailSeller { get; set; }
+        public int? Idpost { get; set; }
+
+        public PurchaseDTO(){}
+        
+        public PurchaseDTO(Product product, Post post, Seller seller, int precio)
+        {
+            this.Precio = precio;
+            Idproducto = product.Id;
+            EmailSeller = seller.Email;
+            Idpost = post.Id;
+        }
+    }
+}
