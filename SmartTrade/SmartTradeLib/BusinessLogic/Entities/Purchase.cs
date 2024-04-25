1444f95a19cc585eb6a547014e54f87ec889fb89
JaviLatorre <javierlatorrecano@gmail.com>
2024-04-25 20:23:03 +0200

    toma push

---
+++ SmartTrade/SmartTradeLib/BusinessLogic/Entities/Purchase.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SmartTrade.BusinessLogic;
+using SmartTrade.Entities;
+
+namespace SmartTrade.Entities
+{
+    public partial class Purchase
+    {
+        protected Purchase()
+        {
+        }
+
+        protected Purchase(Product? product, int price, Seller seller, Post? post) : this()
+        {
+            PurchaseProduct = product;
+            Price = price;
+            PurchaseSeller = seller;
+            PurchasePost = post;
+        }
+    }
+}
