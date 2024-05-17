b08ac1c7fcc75e4a63829cc6ccd6faaed041a060
Flipi2000 <jovacho2000@gmail.com>
2024-05-17 19:46:17 +0200

    Avance de la vista de valoración.

--- SmartTrade/SmartTrade/ViewModels/SendViewModel.cs
+++ SmartTrade/SmartTrade/ViewModels/SendViewModel.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Avalonia.Media.Imaging;
+using Avalonia.Platform;
 
 namespace SmartTrade.ViewModels
 {
     public class SendViewModel
     {
+        public SendViewModel()
+        {
+        }
     }
 }
--- SmartTrade/SmartTrade/Views/SendView.axaml.cs
+++ SmartTrade/SmartTrade/Views/SendView.axaml.cs
@@ -1,12 +1,120 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Media.Imaging;
+using Avalonia.Platform;
+using System;
 
 namespace SmartTrade.Views
 {
     public partial class SendView : UserControl
     {
+        private Bitmap? _starSelected { get; set; }
+        private Bitmap? _starVoid { get; set; }
+        private int _rating;
+
         public SendView()
         {
             InitializeComponent();
+
+            RatingStar1.Click += Star1Click;
+            RatingStar2.Click += Star2Click;
+            RatingStar3.Click += Star3Click;
+            RatingStar4.Click += Star4Click;
+            RatingStar5.Click += Star5Click;
+
+
+            _starSelected = new Bitmap(AssetLoader.Open(new Uri("avares://SmartTrade/Assets/Star.png")));
+            _starVoid = new Bitmap(AssetLoader.Open(new Uri("avares://SmartTrade/Assets/VoidStar.png")));
+        }
+
+        private void Star1Click(object? sender, RoutedEventArgs e)
+        {
+            if ((bool)!RatingStar1.IsChecked)
+            {
+                Star1.Source = _starSelected;
+                Star2.Source = _starVoid;
+                Star3.Source = _starVoid;
+                Star4.Source = _starVoid;
+                Star5.Source = _starVoid;
+
+                _rating = 1;
+            }
+            else
+            {
+                return;
+            }
+        }
+
+        private void Star2Click(object? sender, RoutedEventArgs e)
+        {
+            if ((bool)!RatingStar2.IsChecked)
+            {
+                Star1.Source = _starSelected;
+                Star2.Source = _starSelected;
+                Star3.Source = _starVoid;
+                Star4.Source = _starVoid;
+                Star5.Source = _starVoid;
+
+                _rating = 2;
+            }
+            else
+            {
+                return;
+            }
+        }
+
+        private void Star3Click(object? sender, RoutedEventArgs e)
+        {
+            if ((bool)!RatingStar3.IsChecked)
+            {
+                Star1.Source = _starSelected;
+                Star2.Source = _starSelected;
+                Star3.Source = _starSelected;
+                Star4.Source = _starVoid;
+                Star5.Source = _starVoid;
+
+                _rating = 3;
+            }
+            else
+            {
+                return;
+            }
+        }
+
+        private void Star4Click(object? sender, RoutedEventArgs e)
+        {
+            if ((bool)!RatingStar4.IsChecked)
+            {
+                Star1.Source = _starSelected;
+                Star2.Source = _starSelected;
+                Star3.Source = _starSelected;
+                Star4.Source = _starSelected;
+                Star5.Source = _starVoid;
+
+                _rating = 4;
+            }
+            else
+            {
+                return;
+            }
+        }
+
+        private void Star5Click(object? sender, RoutedEventArgs e)
+        {
+            if ((bool)!RatingStar5.IsChecked)
+            {
+                Star1.Source = _starSelected;
+                Star2.Source = _starSelected;
+                Star3.Source = _starSelected;
+                Star4.Source = _starSelected;
+                Star5.Source = _starSelected;
+
+                _rating = 5;
+            }
+            else
+            {
+                return;
+            }
         }
     }
 }
