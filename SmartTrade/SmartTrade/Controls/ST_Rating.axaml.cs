27e49085ed64eb1f1896977dfc34adeef87a24cd
Flipi2000 <jovacho2000@gmail.com>
2024-05-22 01:22:14 +0200

    ST_Rating creado y añadida una fila mas en product view

---
+++ SmartTrade/SmartTrade/Controls/ST_Rating.axaml.cs
@@ -0,0 +1,14 @@
+using Avalonia.Controls;
+
+namespace SmartTrade.Controls
+{
+    public partial class ST_Rating : UserControl
+    {
+
+        public ST_Rating()
+        {
+            InitializeComponent();
+            
+        }
+    }
+}
