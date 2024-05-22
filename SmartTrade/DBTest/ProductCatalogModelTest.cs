34f5ff133023bd460eea81075f64fb9789315325
JaviLatorre <javierlatorrecano@gmail.com>
2024-05-22 13:23:20 +0200

    Cambioa

--- SmartTrade/DBTest/ProductCatalogModelTest.cs
+++ SmartTrade/DBTest/ProductCatalogModelTest.cs
@@ -9,11 +9,13 @@ using SmartTrade.ViewModels;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace SmartTrade.Tests
+namespace SmartTrade.nUnitTests
 {
     [TestFixture]
     public class ProductCatalogModelTest
     {
+        ProductCatalogModel _model = new ProductCatalogModel();
+
         [SetUp]
         public void SetUp()
         {
@@ -29,7 +31,64 @@ namespace SmartTrade.Tests
                              new SimplePostDTO { EcologicPrint = "8", Category = "Electrónica", SellerID = "456", ProductName = "Producto 2", Title = "Titulo 2" }
                          });
         }
-       
+
+        [TestMethod]
+        public void IsEcologic_ShouldReturnTrue_WhenEcologicPrintIsLessThanTen()
+        {
+            var post = new SimplePostDTO { EcologicPrint = "9" };
+            var result = _model.IsEcologic(post);
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void IsEcologic_ShouldReturnFalse_WhenEcologicPrintIsTenOrMore()
+        {
+            var post = new SimplePostDTO { EcologicPrint = "10" };
+
+            var result = _model.IsEcologic(post);
+
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void CalculateProductNameScore_ShouldReturnCorrectScore_WhenSimilarityExceedsThreshold()
+        {
+            var productNamePost = "Producto";
+            var namePurchase = "Prod";
+            var threshold = 20;
+            var score = _model.CalculateProductNameScore(productNamePost, namePurchase, threshold);
+            Assert.Greater(score, 0);
+        }
+
+        [TestMethod]
+        public void CalculateCategoryAndSellerScore_ShouldReturnCorrectScore_WhenBothMatch()
+        {
+            var categoryPost = new Category("Electrónica");
+            var categoryPurchase = new Category("Electrónica");
+            var sellerIdPost = "123";
+            var emailSellerPurchase = "123@example.com";
+            var score = _model.CalculateCategoryAndSellerScore(categoryPost, categoryPurchase, sellerIdPost, emailSellerPurchase);
+            Assert.AreEqual(1.00f, score);
+        }
+        [TestMethod]
+        public void IsRelated_ShouldReturnTrue_WhenCriteriaMet()
+        {
+            var post = new SimplePostDTO { EcologicPrint = "9", Category = "Electrónica", SellerID = "123", ProductName = "Producto 1", Title = "Titulo 1" };
+            _mockService.Setup(s => s.Purchases).Returns(EcologicPrint = "9", Category = "Electrónica", SellerID = "123", ProductName = "Producto 1", Title = "Titulo 1");
+            var result = _model.IsRelated(post);
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void IsRelated_ShouldReturnFalse_WhenCriteriaNotMet()
+        {
+            var post = new SimplePostDTO { EcologicPrint = "9", Category = "Electrónica", SellerID = "123", ProductName = "Producto 1", Title = "Titulo 1" };
+            _mockService.Setup(s => s.Purchases).Returns(EcologicPrint = "1", Category = "Juguete", SellerID = "hola", ProductName = "hola", Title = "hola");
+            var result = _model.IsRelated(post);
+            Assert.IsFalse(result);
+        }
+
+
 
     }
 }
